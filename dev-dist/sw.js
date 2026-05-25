2133d3069e8895a8255c644130eb4d4a12ef82d4
Alex16oz <85593164+Alex16oz@users.noreply.github.com>
2026-05-25 23:25:56 +0700

    v6 test

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0ddji96b1to"
+    "revision": "0.upbtp09gj2g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
